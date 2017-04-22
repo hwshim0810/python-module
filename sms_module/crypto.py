4c45fcb39ff86566dfe879257d44d2c6d2c581b3
Shim.hw <shim.hw0810@gmail.com>
2017-04-22 22:52:48 +0900

    Add pycrypto module

---
+++ sms_module/crypto.py
@@ -0,0 +1,44 @@
+import base64
+from Crypto.Cipher import AES
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS).encode()
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+
+# 초기화
+def iv():
+    """
+    The initialization vector to use for encryption or decryption.
+    It is ignored for MODE_ECB and MODE_CTR.
+    """
+    return chr(0) * 16
+
+
+class AESCipher(object):
+    """
+    https://github.com/dlitz/pycrypto
+    """
+
+    def __init__(self, key):
+        self.key = key
+        # self.key = hashlib.sha256(key.encode()).digest()
+
+    # Message encrypt
+    def encrypt(self, message):
+        """
+        It is assumed that you use Python 3.0+
+        , so plaintext's type must be str type(== unicode).
+        """
+        message = message.encode()
+        raw = pad(message)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv())
+        enc = cipher.encrypt(raw)
+        return base64.b64encode(enc).decode('utf-8')
+
+    # Message decrypt
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv())
+        dec = cipher.decrypt(enc)
+        return unpad(dec).decode('utf-8')
