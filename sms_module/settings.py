3f85695a7be1d7a795bfd9e16c5fa741accc9dc2
Shim.hw <shim.hw0810@gmail.com>
2017-04-22 15:10:03 +0900

    Seperate method

--- sms_module/settings.py
+++ sms_module/settings.py
@@ -11,20 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import json
-from django.core.exceptions import ImproperlyConfigured
-
-with open("key.json") as f:
-    key = json.loads(f.read())
-
-
-# Keep secret keys in secrets.json
-def get_secret(setting, key=key):
-    try:
-        return key[setting]
-    except KeyError:
-        error_msg = "Set the {0} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+from sms_module.util import get_secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
