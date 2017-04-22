3f83e16bb17f15b47ff2dd321b8f25b7c45eb2ec
Shim.hw <shim.hw0810@gmail.com>
2017-04-22 17:38:42 +0900

    Create send module

---
+++ tel_active/coolmsg.py
@@ -0,0 +1,27 @@
+from sdk.api.message import Message
+from sdk.exceptions import CoolsmsException
+from sms_module.util import get_secret
+
+# set api key, api secret
+api_key = get_secret("api_key")
+api_secret = get_secret("api_secret")
+
+
+def sms_send(to_num, r_num):
+    params = dict()
+    params['type'] = 'sms'
+    params['to'] = to_num
+    params['from'] = get_secret("from_num")
+    params['text'] = str(r_num)
+
+    cool = Message(api_key, api_secret)
+    print(params)
+    try:
+        res = cool.send(params)
+        res['r_num'] = r_num
+        return res
+
+    except CoolsmsException as e:
+        print("Error Code : %s" % e.code)
+        print("Error Message : %s" % e.msg)
+        raise e
