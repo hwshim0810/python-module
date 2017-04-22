bc3994b5cbda9d4b7bdc368dcb5dd074bc406f7a
Shim.hw <shim.hw0810@gmail.com>
2017-04-22 23:23:32 +0900

    Add models

--- tel_active/models.py
+++ tel_active/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class AuthInfo(models.Model):
+    phone = models.CharField(max_length=50)
+    msg = models.CharField(max_length=20)
+    counter = models.IntegerField(default=0)
+    date = models.DateField(default=timezone.now)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.phone
